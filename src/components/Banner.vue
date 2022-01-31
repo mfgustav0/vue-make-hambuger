28fbb37017188554af05cb0973eea030e224a8c1
mfgustav0 <gustavomonteirofavaro@gmial.com>
2022-01-30 23:55:45 -0300

    commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="main-barner">
+		<h1>Make Your Burger</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Banner'
+	}
+</script>
+
+<style scoped>
+	#main-barner {
+		background-image: url('/img/burger.jpg');
+		background-position: 0;
+		background-size: cover;
+		display: flex;
+		height: 500px;
+		align-items: center;
+		justify-content: flex-start;
+	}
+
+	#main-barner h1 {
+		color: #FFF;
+		text-align: center;
+		font-size: 60px;
+		background-color: #222;
+		padding: 20px 40px;
+	}
+</style>
